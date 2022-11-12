77469118acdebd241a170770e835018b1dcdcd99
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-12 09:48:23 -0500

    labels space fixed

--- src/issues/composables/useLabels.ts
+++ src/issues/composables/useLabels.ts
@@ -7,7 +7,7 @@ import { Label } from '../interfaces/label';
 
 const getLabels = async():Promise<Label[]> => {
 
-  const { data } = await githubApi<Label[]>('/labels?per_page=100', {
+  const { data } = await githubApi<Label[]>('/labels?per_page=10', {
     headers: { Authorization: null },
   });
   return data;
